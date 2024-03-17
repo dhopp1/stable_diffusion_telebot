2222d1b8a81328c58ac201b5b982e8fdbdf3545c
dhopp1 <daniel.hopp1@gmail.com>
2024-03-17 18:50:49 +0100

    Adding img2img

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import random
 import torch
 
 from helper.txt2img import initialize_txt2img, gen_txt2img
+from helper.img2img import initialize_img2img, gen_img2img
+
 
 # bot setup
 BOT_TOKEN = sys.argv[1]
@@ -44,13 +46,14 @@ else:
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if device == "cuda" else None
 
-num_variations = 20
+num_variations = 1
 num_inference_steps = 20
 guidance_scale = 7.5
 height = 512
 width = 512
 manual_seeds = None
 negative_prompt = "out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature."
+strength = 0.5
 
 
 @bot.message_handler(commands=["start", "hello"])
@@ -60,10 +63,16 @@ def send_welcome(message):
     )
 
     if message.from_user.id in admins:
-        global xl_pipe
+        global txt2img_pipe
+        global img2img_pipe
 
-        # xl model
-        xl_pipe = initialize_txt2img(
+        # txt2img model
+        txt2img_pipe = initialize_txt2img(
+            model_name, model_list.loc[0, "path"], device, torch_dtype
+        )
+        
+        # img2img model
+        img2img_pipe = initialize_img2img(
             model_name, model_list.loc[0, "path"], device, torch_dtype
         )
 
@@ -78,55 +87,97 @@ height = {height} (height in pixels of output image)
 width = {width} (width in pixels of output image)
 manual_seeds = {manual_seeds} (list of integers as long as 'num_variations', in case you want to create the same image again)
 negative_prompt = '{negative_prompt}' (what you don't want in the image')
+
 To change these, pass the following as your prompt: '[params]{{'prompt':'prompt text', 'num_variations':2, 'manual_seeds':[4,7]}}', etc.
 """,
         )
     else:
         bot.reply_to(message, "Unauthorized user")
 
-
-@bot.message_handler(func=lambda msg: True)
+# txt2img handler
+@bot.message_handler(func=lambda msg: True, content_types = ["photo", "document", "text"])
 def echo_all(message):
     bot.send_message(
         message.chat.id,
         text=f"generating...",
     )
+    
+    message_text = message.text if message.text is not None else message.caption
 
     # parameters in message
-    if "[params]" in message.text:
+    if "[params]" in message_text:
         try:
-            param_dict = eval(message.text.split("params]")[1])
+            param_dict = eval(message_text.split("params]")[1])
         except:
             bot.send_message(message.chat.id, text="prompt format error, try again")
     else:
-        param_dict = {"prompt": message.text}
+        param_dict = {"prompt": message_text}
 
-    try:
-        img_paths = gen_txt2img(
-            pipe=xl_pipe,
-            prompt=param_dict["prompt"],
-            device=device,
-            image_name=param_dict["image_name"]
-            if "image_name" in param_dict.keys()
-            else "output",
-            num_variations=param_dict["num_variations"]
-            if "num_variations" in param_dict.keys()
-            else num_variations,
-            num_inference_steps=param_dict["num_inference_steps"]
-            if "num_inference_steps" in param_dict.keys()
-            else num_inference_steps,
-            guidance_scale=param_dict["guidance_scale"]
-            if "guidance_scale" in param_dict.keys()
-            else guidance_scale,
-            height=param_dict["height"] if "height" in param_dict.keys() else height,
-            width=param_dict["width"] if "width" in param_dict.keys() else width,
-            manual_seeds=param_dict["manual_seeds"]
-            if "manual_seeds" in param_dict.keys()
-            else manual_seeds,
-            negative_prompt=param_dict["negative_prompt"]
-            if "negative_prompt" in param_dict.keys()
-            else negative_prompt,
-        )
+    if True:
+        # img2img
+        if message.document is not None:
+            file_info = bot.get_file(message.document.file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+            with open("metadata/input_images/tmp_image.png", 'wb') as new_file:
+                new_file.write(downloaded_file)
+                new_file.close()
+                
+            img_paths = gen_img2img(
+                pipe=img2img_pipe,
+                prompt=param_dict["prompt"],
+                device=device,
+                image_name=param_dict["image_name"]
+                if "image_name" in param_dict.keys()
+                else "output",
+                num_variations=param_dict["num_variations"]
+                if "num_variations" in param_dict.keys()
+                else num_variations,
+                num_inference_steps=param_dict["num_inference_steps"]
+                if "num_inference_steps" in param_dict.keys()
+                else num_inference_steps,
+                guidance_scale=param_dict["guidance_scale"]
+                if "guidance_scale" in param_dict.keys()
+                else guidance_scale,
+                height=param_dict["height"] if "height" in param_dict.keys() else height,
+                width=param_dict["width"] if "width" in param_dict.keys() else width,
+                strength=param_dict["strength"]
+                if "strength" in param_dict.keys()
+                else strength,
+                manual_seeds=param_dict["manual_seeds"]
+                if "manual_seeds" in param_dict.keys()
+                else manual_seeds,
+                negative_prompt=param_dict["negative_prompt"]
+                if "negative_prompt" in param_dict.keys()
+                else negative_prompt,
+            )
+                
+        else:
+            # txt2img
+            img_paths = gen_txt2img(
+                pipe=txt2img_pipe,
+                prompt=param_dict["prompt"],
+                device=device,
+                image_name=param_dict["image_name"]
+                if "image_name" in param_dict.keys()
+                else "output",
+                num_variations=param_dict["num_variations"]
+                if "num_variations" in param_dict.keys()
+                else num_variations,
+                num_inference_steps=param_dict["num_inference_steps"]
+                if "num_inference_steps" in param_dict.keys()
+                else num_inference_steps,
+                guidance_scale=param_dict["guidance_scale"]
+                if "guidance_scale" in param_dict.keys()
+                else guidance_scale,
+                height=param_dict["height"] if "height" in param_dict.keys() else height,
+                width=param_dict["width"] if "width" in param_dict.keys() else width,
+                manual_seeds=param_dict["manual_seeds"]
+                if "manual_seeds" in param_dict.keys()
+                else manual_seeds,
+                negative_prompt=param_dict["negative_prompt"]
+                if "negative_prompt" in param_dict.keys()
+                else negative_prompt,
+            )
         # send the image
         for img_path in img_paths:
             bot.send_message(
@@ -136,7 +187,9 @@ def echo_all(message):
             bot.send_photo(message.chat.id, img)
             img.close()
             os.remove(img_path)  # delete the image locally
-    except:
+        if os.path.exists("metadata/input_images/tmp_image.png"):
+            os.remove("metadata/input_images/tmp_image.png")
+    else:
         bot.send_message(message.chat.id, text="error encountered, please try again")
 
 
